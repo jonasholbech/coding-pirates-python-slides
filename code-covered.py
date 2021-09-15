8e18b7654dcd9b101cc0649460928098bbf100c3
Jonas Holbech <jh@jonasholbech.dk>
2021-09-15 14:05:52 +0200

    setBlocks

---
+++ code-covered.py
@@ -0,0 +1,29 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import mcpi.entity as entity
+import time
+mc = minecraft.Minecraft.create()
+
+#teleporter spilleren til 0,0,0
+mc.player.setTilePos(0,0,0)
+
+#skriv en besked i chatten
+mc.postToChat("Hej Verden")
+            
+#Sæt en træ-blok
+mc.setBlock(x, y, z, block.WOOD.id)
+
+#spawn en "entity"
+mc.spawnEntity(0, 2, 0, entity.CHICKEN.id)
+
+#lav en variabel (pos)
+#der indeholder spillerens koordinater
+pos = mc.player.getPos()
+
+mc.setBlocks(x1, y1, z1, x2, y2, z2, block.WOOD.id)
+     
+
+while True:
+      pos = mc.player.getPos()
+      mc.setBlock(pos.x, pos.y, pos.z, block.TNT.id)
+      time.sleep(0.1)
